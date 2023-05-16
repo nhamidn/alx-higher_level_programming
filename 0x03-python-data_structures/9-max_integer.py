7843d09230e133344dba10c76af5938406825847
nhamidn <hamid-savana@hotmail.fr>
2023-05-16 20:58:21 +0100

    fix error

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list is None or len(my_list) == 0:
         return None
-    biggest = 0
+    biggest = my_list[0]
     for element in my_list:
         if element > biggest:
             biggest = element
