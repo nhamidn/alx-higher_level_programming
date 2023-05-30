5b010cad7653286226f0817d438c9b1a67177287
nhamidn <hamid-savana@hotmail.fr>
2023-05-30 23:34:09 +0100

    mandatory 3.0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""File that define a square class."""
+
+
 class Square:
     """Empty class that defines a square"""
     pass
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""File that define a square class."""
+
+
 class Square:
     """A class that defines a square"""
     def __init__(self, size=0, position=(0, 0)):
