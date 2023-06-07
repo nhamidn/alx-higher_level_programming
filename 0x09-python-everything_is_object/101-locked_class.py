0409f4db5eaffd585ba7c65564d77d914e9100de
nhamidn <hamid-savana@hotmail.fr>
2023-06-07 15:25:23 +0100

    new bonus

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""File that define a LockedClass."""
+
+
+class LockedClass:
+    """A class that control the creation of new instance attributes"""
+    __slots__ = ['first_name']
