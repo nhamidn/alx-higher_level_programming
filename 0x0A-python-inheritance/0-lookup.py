7bf7c3f2c8cbcfa172e457d8e069e0152be3754a
nhamidn <hamid-savana@hotmail.fr>
2023-06-13 00:05:37 +0100

    first

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A file that defines lookpu method
+"""
+
+
+def lookup(obj):
+    """function that returns the attributes and methods of an object"""
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A file that define a class MyList
+"""
+
+
+class MyList(list):
+    """Class with method print_sorted"""
+    pass
+
+    def print_sorted(self):
+        """Methot that sorted a list"""
+
+        print(sorted(list(self)))
