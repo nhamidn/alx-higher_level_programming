df5d5689dedf5028c29f57a8ad3172859875a9ff
nhamidn <hamid-savana@hotmail.fr>
2023-06-14 23:33:04 +0100

    fixes

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,10 +12,9 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Init method of the Square class"""
-
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(self.__size, self.__size)
 
     def area(self):
         """Method that calulate the area of a Square"""
