438b50e120e1f5d36554868023848382fcd46ad7
nhamidn <hamid-savana@hotmail.fr>
2023-07-20 00:08:41 +0100

    another test

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Print all privileges of user_0d_1 and user_0d_2
--- Execute: cat 0-privileges.sql | mysql -hlocalhost -uroot -p
-SHOW GRANTS FOR `user_0d_1`@`localhost`;
-SHOW GRANTS FOR `user_0d_2`@`localhost`;
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
