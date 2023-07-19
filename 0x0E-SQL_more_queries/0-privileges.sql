def3986f5cf54c794be4409d46e9ac19d2a3b185
nhamidn <hamid-savana@hotmail.fr>
2023-07-19 23:56:26 +0100

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Print all privileges of user_0d_1 and user_0d_2
--- > cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+-- Execute: cat 0-privileges.sql | mysql -hlocalhost -uroot -p
 SHOW GRANTS FOR `user_0d_1`@`localhost`;
 SHOW GRANTS FOR `user_0d_2`@`localhost`;
