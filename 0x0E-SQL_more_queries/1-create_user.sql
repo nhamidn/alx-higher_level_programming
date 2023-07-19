08505b476f3a42cd2cca38372481e885cdcf8c25
nhamidn <hamid-savana@hotmail.fr>
2023-07-20 00:28:45 +0100

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
--- creates the MySQL server user user_0d_1
+-- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- grant privileges to user_0d_1
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+-- Grant all privileges to user_0d_1 on the entire MySQL server
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
