0acb97540f15e9a768b6d38ae09bb68a3a4c4da1
nhamidn <hamid-savana@hotmail.fr>
2023-08-31 22:05:39 +0100

    2 and 3

---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Script that displays the body of the response
+curl -sX DELETE $1 -L
---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Script that displays all the methods accepted by the server
+curl -sI ALLOW $1 | grep "Allow" | cut -d " " -f2-
