2a5b4f142d0afe0cf728711689dde571b624447a
nhamidn <hamid-savana@hotmail.fr>
2023-09-02 18:17:52 +0100

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script that lists 10 commits of the repository rails"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """Program that lists 10 commits of the repository"""
+    if len(argv) != 3:
+        print("Usage: ./100-github_commits.py <repository name> <owner>")
+        exit(1)
+    repo = argv[1]
+    owner = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    r = requests.get(url)
+    result = r.json()
+    print(result)
+    for i in range(10):
+        print("{}: {}".format(result[i].get('sha'), result[i].
+                              get('commit').get('author').get('name')))
