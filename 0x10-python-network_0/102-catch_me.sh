90ee7ec49a6b669ac866c9c4c1ee0053154facfe
nhamidn <hamid-savana@hotmail.fr>
2023-09-02 20:11:37 +0100

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that causes the server to respond with a message
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -s -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me -L
