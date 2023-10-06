39f1072c496fe1f887ddbad3118fb02442a1081f
nhamidn <hamid-savana@hotmail.fr>
2023-10-06 22:12:45 +0100

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (error, _response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const completedTasks = {};
+    body = JSON.parse(body);
+
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasks[userId]) {
+        completedTasks[userId] = 0;
+      }
+
+      if (completed) ++completedTasks[userId];
+    }
+
+    console.log(completedTasks);
+  }
+});
