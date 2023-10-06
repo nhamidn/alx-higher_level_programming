b8d1474d5686d73f2b8c62f52c994b65a2b3b0ac
nhamidn <hamid-savana@hotmail.fr>
2023-10-06 10:31:28 +0100

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(starWarsUri, function (_error, _response, body) {
+  body = JSON.parse(body);
+  console.log(body.title);
+});
